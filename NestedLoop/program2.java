67486096c2137b4617a70fb17333404af63fe9b8
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2025-12-04 21:46:05 +0530

    Add files via upload

--- NestedLoop/program2.java
+++ NestedLoop/program2.java
@@ -2,13 +2,16 @@ import java.util.*;
 class PatternDemo2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num= 1;
         System.out.println("Enter the no. of rows : ");
         int r = sc.nextInt();
         for(int i=1;i<=r;i++){
             for(int j=1;j<=r;j++){
-                System.out.print("C2W"+" ");
+                System.out.print(num*num+" ");
+            num++;
             } System.out.println();
-        }
+            
+        } 
     }
     
     }
