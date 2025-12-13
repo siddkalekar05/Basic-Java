582a352dd7b2c569191aca03ef23674e9eb5d4af
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2025-12-13 21:09:48 +0530

    Add files via upload

---
+++ OOPS/Constructor/program1.java
@@ -0,0 +1,11 @@
+// Intance var 
+class InstVar{
+	      int x = 10;
+	      int y = 20;
+	public static void main(String[] args){
+		InstVar obj = new InstVar();
+		System.out.println(obj.x);
+		System.out.println(obj.y);
+	}
+}
+
---
+++ OOPS/Constructor/program5.java
@@ -0,0 +1,9 @@
+// Contructor without arg
+class Demo{
+	Demo(){
+		System.out.println("In contructor");
+		}
+	public static void main(String[] args){
+		Demo obj = new Demo();
+	}
+}
