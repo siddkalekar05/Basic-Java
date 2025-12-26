8b19b1a98cfea67a55c95cd94baeb77e2ad175e3
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2025-12-26 23:20:56 +0530

    Add files via upload

---
+++ Collection/Set/program1.java
@@ -0,0 +1,14 @@
+//Set
+import java.util.*;
+
+class SetDemo1{
+    public static void main(String args[]){
+       Set obj = new HashSet<>();
+
+       obj.add("A");
+       obj.add("B");
+       obj.add("C");
+
+       System.out.println(obj);
+    }
+} 
