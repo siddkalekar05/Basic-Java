9722e62acef27e4e3501ab0f67e8e7a57bb13902
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2025-12-27 21:44:41 +0530

    Add files via upload

---
+++ Collection/List/program1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class ListDemo1 {
+    public static void main(String args[]){
+
+        List<Object> ll = new ArrayList<>();
+
+        ll.add(10);
+        ll.add(11.5);
+        ll.add(20);
+        ll.add(20.5f);
+        ll.add("SIDD");
+        ll.add("A");
+        ll.add(null);
+        System.out.println(ll);
+    }
+}
+
