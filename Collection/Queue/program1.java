609978aa27cce700aeeef6309fd537a302f45bc4
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2025-12-31 22:34:00 +0530

    Add files via upload

---
+++ Collection/Queue/program1.java
@@ -0,0 +1,17 @@
+//Queue
+
+import java.util.*;
+
+class QueueDemo {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        System.out.println("Queue: " + q);
+    }
+}
+
